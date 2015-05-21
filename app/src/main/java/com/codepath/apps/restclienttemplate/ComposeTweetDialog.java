9bfe40f7517864113a372c3e156617f91f1ac99b
Teddy Wyly <wylynout@gmail.com>
2015-05-21 15:29:03 -0400

    character count implemented

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeTweetDialog.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeTweetDialog.java
@@ -2,14 +2,25 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by teddywyly on 5/19/15.
  */
 public class ComposeTweetDialog extends DialogFragment {
+
+    private EditText etTweet;
+    private TextView tvCount;
+    private Button btnTweet;
+    private Button btnCancel;
+    private int textCount;
 
     public ComposeTweetDialog() {
 
@@ -27,8 +38,48 @@ public class ComposeTweetDialog extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_compose_tweet, container);
-//        String title = getArguments().getString("title", "Enter Name");
-//        getDialog().setTitle(title);
+        etTweet = (EditText) view.findViewById(R.id.etTweet);
+        tvCount = (TextView) view.findViewById(R.id.tvCount);
+        textCount = Integer.parseInt(tvCount.getText().toString());
+
+        btnTweet = (Button) view.findViewById(R.id.btnTweet);
+        btnCancel = (Button) view.findViewById(R.id.btnCancel);
+
+        setupButtonListeners();
+
+        etTweet.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int count = textCount - s.length();
+                tvCount.setText(Integer.toString(count));
+            }
+        });
         return view;
+    }
+
+    private void setupButtonListeners() {
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
     }
 }
